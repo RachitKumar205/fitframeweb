d2b505afa1e32368271a57e7f0820c2e1869377b
RachitKumar205 <rk_programmer@outlook.com>
2021-07-29 20:01:16 +0530

    Added whitenoise

--- fitweb/asgi.py
+++ fitweb/asgi.py
@@ -14,10 +14,12 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+from fitweb import settings
 from main.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitweb.settings')
 django.setup()
+settings.configure()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
