e78d80205809ff133c341816ddb49ffc6701d8b4
RachitKumar205 <rk_programmer@outlook.com>
2021-07-31 13:50:02 +0530

    Removed channel layers

--- fitweb/settings.py
+++ fitweb/settings.py
@@ -82,11 +82,6 @@ ASGI_APPLICATION = 'fitweb.asgi.application'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
